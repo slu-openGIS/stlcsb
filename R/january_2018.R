3fd5b0eacef9f4a8904ee0d756ab2cdd3dbe4858
bransonf <32143148+bransonf@users.noreply.github.com>
2018-09-23 14:35:13 -0500

    better documentation

--- R/january_2018.R
+++ R/january_2018.R
@@ -8,25 +8,25 @@
 #'
 #' @format A tibble with 8812 rows and 19 variables:
 #' \describe{
-#'   \item{REQUESTID}{unique request identifier}
-#'   \item{PROBLEMCODE}{code of the request}
-#'   \item{DESCRIPTION}{description of the request, not significantly different from PROBLEMCODE}
+#'   \item{REQUESTID}{system generated unique request identifier}
+#'   \item{PROBLEMCODE}{type of report}
+#'   \item{DESCRIPTION}{same as problemcode OR slightly more specific}
 #'   \item{PROBADDRESS}{address of the request}
-#'   \item{PROBCITY}{city of the request, note that all these data are for St. Louis}
-#'   \item{PROBZIP}{zip code of the request}
-#'   \item{PROBADDTYPE}{A or B, undetermined what this means}
-#'   \item{SUBMITTO}{department the request was submitted to}
-#'   \item{DATETIMEINIT}{date and time the request was recorded}
+#'   \item{PROBCITY}{city of the request, note that all these data are for the City of St. Louis}
+#'   \item{PROBZIP}{zip code of the request, often blank}
+#'   \item{PROBADDTYPE}{A = Parcel, B = Intersection}
+#'   \item{SUBMITTO}{city division responsible for completing the request}
+#'   \item{DATETIMEINIT}{date and time the request was initiated}
 #'   \item{DATETIMECLOSED}{date and time the request was closed}
-#'   \item{SRX}{stateplane meters X-axis}
-#'   \item{SRY}{stateplane meters Y-axis}
-#'   \item{STATUS}{if the request is open or closed}
-#'   \item{PRJCOMPLETEDATE}{date the request was completed}
-#'   \item{DATECANCELLED}{date the request was closed}
-#'   \item{CALLERTYPE}{unknown, insufficient data}
-#'   \item{DATEINVTDONE}{unknown}
-#'   \item{NEIGHBORHOOD}{neighborhood of the request}
-#'   \item{WARD}{ward of the request}
+#'   \item{SRX}{map coordinate, X-coordinate}
+#'   \item{SRY}{map coordinate, Y-coordinate}
+#'   \item{STATUS}{status of the request}
+#'   \item{PRJCOMPLETEDATE}{date by which city division should have initial inspection complete, auto-populated based on service level agreements}
+#'   \item{DATECANCELLED}{indicates a duplicate, cancelled or entered in error request}
+#'   \item{CALLERTYPE}{method used by citizen to report issue (Phone, Web, Twitter, etc)}
+#'   \item{DATEINVTDONE}{date of investigation-date that work was done, may differ from closing date because of crews using paper copies of requests}
+#'   \item{NEIGHBORHOOD}{City of St. Louis Neighborhood number (1-79)}
+#'   \item{WARD}{City of St. Louis Ward number (1-28)}
 #'   }
 #'
 #' @source \href{https://www.stlouis-mo.gov/data/service-requests.cfm}{St. Louis Citizens' Service Bureau}
