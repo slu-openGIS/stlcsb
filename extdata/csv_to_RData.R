cb11137254dab3f573aba99961be2c433221f26d
bransonf <32143148+bransonf@users.noreply.github.com>
2018-10-18 00:41:35 -0500

    clean definitions func

--- extdata/csv_to_RData.R
+++ extdata/csv_to_RData.R
@@ -1,26 +1,25 @@
-#.csv to RData
+#csv to rda
 
 library(readr)
-library(here)
 
 # import the csv data
 
-Admin <- read_csv(here("extdata/Admin.csv"), col_names = FALSE)
-Animal <- read_csv(here("extdata/Animal.csv"), col_names = FALSE)
-Construction <- read_csv(here("extdata/Construction.csv"), col_names = FALSE)
-Debris <- read_csv(here("extdata/Debris.csv"), col_names = FALSE)
-Degrade <- read_csv(here("extdata/Degrade.csv"), col_names = FALSE)
-Disturbance <- read_csv(here("extdata/Disturbance.csv"), col_names = FALSE)
-Event <- read_csv(here("extdata/Event.csv"), col_names = FALSE)
-Health <- read_csv(here("extdata/Health.csv"), col_names = FALSE)
-Landscape <- read_csv(here("extdata/Landscape.csv"), col_names = FALSE)
-Law <- read_csv(here("extdata/Law.csv"), col_names = FALSE)
-Maintenance <- read_csv(here("extdata/Maintenance.csv"), col_names = FALSE)
-Nature <- read_csv(here("extdata/Nature.csv"), col_names = FALSE)
-Road <- read_csv(here("extdata/Road.csv"), col_names = FALSE)
-Sewer <- read_csv(here("extdata/Sewer.csv"), col_names = FALSE)
-Traffic <- read_csv(here("extdata/Traffic.csv"), col_names = FALSE)
-Waste <- read_csv(here("extdata/Waste.csv"), col_names = FALSE)
+Admin <- read_csv("extdata/Admin.csv", col_names = FALSE)
+Animal <- read_csv("extdata/Animal.csv", col_names = FALSE)
+Construction <- read_csv("extdata/Construction.csv", col_names = FALSE)
+Debris <- read_csv("extdata/Debris.csv", col_names = FALSE)
+Degrade <- read_csv("extdata/Degrade.csv", col_names = FALSE)
+Disturbance <- read_csv("extdata/Disturbance.csv", col_names = FALSE)
+Event <- read_csv("extdata/Event.csv", col_names = FALSE)
+Health <- read_csv("extdata/Health.csv", col_names = FALSE)
+Landscape <- read_csv("extdata/Landscape.csv", col_names = FALSE)
+Law <- read_csv("extdata/Law.csv", col_names = FALSE)
+Maintenance <- read_csv("extdata/Maintenance.csv", col_names = FALSE)
+Nature <- read_csv("extdata/Nature.csv", col_names = FALSE)
+Road <- read_csv("extdata/Road.csv", col_names = FALSE)
+Sewer <- read_csv("extdata/Sewer.csv", col_names = FALSE)
+Traffic <- read_csv("extdata/Traffic.csv", col_names = FALSE)
+Waste <- read_csv("extdata/Waste.csv", col_names = FALSE)
 
 # we then want to unlist our data, meaning that it will be vectorized. This is neccessary for the later use of dplyr mutate.
 
@@ -43,5 +42,5 @@ Waste <- unlist(Waste)
 
 # then to save as Rdata
 
-save(Admin, Animal, Construction, Debris, Degrade, Disturbance, Event, Health, Landscape, Law, Maintenance, Nature, Road, Sewer, Traffic, Waste, file = here("data/definitions.RData"))
+save(Admin, Animal, Construction, Debris, Degrade, Disturbance, Event, Health, Landscape, Law, Maintenance, Nature, Road, Sewer, Traffic, Waste, file = "data/definitions.rda")
 
