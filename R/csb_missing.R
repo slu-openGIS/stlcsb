42b8e2408f75bcd48fae29b6037149bab7f0df3d
bransonf <32143148+bransonf@users.noreply.github.com>
2018-10-25 20:20:11 -0500

    notes

--- R/csb_missing.R
+++ R/csb_missing.R
@@ -10,8 +10,9 @@
 #' @param newVar if named, produces a new column with a logical indicator of incomplete spatial data (For x OR y)
 #' @param filter if true, returns a filtered version of the data with only observations with complete spatial data
 #'
-#' @return \code{csb_missing} returns a logical vector indicating `TRUE` if an observation has incomplete spatial data.
+#' @return \code{csb_missing} returns a logical vector indicating `TRUE` if an observation has incomplete spatial data, or a filtered version of the input data
 #'
+#' @importFrom dplyr mutate
 #' @importFrom dplyr filter
 #' @importFrom rlang :=
 #' @importFrom rlang quo
@@ -29,33 +30,47 @@ csb_missing <- function(.data, varX, varY, newVar, filter = FALSE){
   if (!is.character(paramList$varX)) {
     varXN <- rlang::enquo(varX)
   }
-  else if (is.character(paramList$var)) {
+  else if (is.character(paramList$varX)) {
     varXN <- rlang::quo(!! rlang::sym(varX))
   }
-  if (!is.character(paramList$varX)) {
+  if (!is.character(paramList$varY)) {
     varYN <- rlang::enquo(varY)
   }
-  else if (is.character(paramList$var)) {
+  else if (is.character(paramList$varY)) {
     varYN <- rlang::quo(!! rlang::sym(varY))
   }
+
   newVarN <- rlang::quo_name(rlang::enquo(newVar))
 
+  ### METHODS: Is it fair to assume that nchar() < 6 is invalid???
+  ## Found some edge cases that need to be accounted for... for example srx = -10050058.4
 
-
+  ## if newVar is named
   # mutate function
+  if(newVarN != ""){.data %>%
+      dplyr::mutate(!!newVarN := dplyr::if_else((nchar(!!varXN) >= 6), FALSE, TRUE)) -> out #this is the test, if SRX and SRY are not both >=6, return TRUE for 'missing'
+    # need addition to check for SRY, but not to ever replace a TRUE with FALSE...
+  #&&(nchar(!!varYN) >= 6)
 
-  if (nchar(as.numeric(varX)) < 6){mutate(.data, newVar = TRUE)}
-  if (nchar(as.numeric(varY)) < 6){return(TRUE)}
+    # edge case, showing false 900000,
+    # need to quote before checking length '900000' is 6 char
+    #cause of the issue is that more than 4 zeros produces sci notation
 
+  }
+  #if (nchar(as.numeric(!!varXN)) < 6){mutate(.data, newVar = TRUE)}
+  #if (nchar(as.numeric(!!varYN)) < 6){return(TRUE)}
 
+  ## if filter is true
   # The filter function
   if (isTRUE(filter)){.data %>%
-  dplyr::filter(as.numeric(varX) >=6) %>%
-  dplyr::filter(as.numeric(varY) >=6) -> filtered
-  return(filtered)
+  dplyr::filter(nchar(!!varXN) >=6) %>%
+  dplyr::filter(nchar(!!varYN) >=6) -> filtered
   }
 
   ## return based on filter argument
   if(isTRUE(filter)){return(filtered)}
-  else if(isFALSE(filter)&&is.character(newVarN)) return(out)) ##is.charcater may not work because of NSE here
+  else if(isFALSE(filter)&&newVarN != ""){return(out)} ##is.charcater may not work because of NSE here
+
+  ## would like to return a message with the count of observations removed.
 }
+
