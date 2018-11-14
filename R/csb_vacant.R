6bf8ff4a35c96c5d2a84db8604967bef70f9cb3a
bransonf <32143148+bransonf@users.noreply.github.com>
2018-11-14 10:18:39 -0600

    vacant function done

--- R/csb_vacant.R
+++ R/csb_vacant.R
@@ -1,18 +1,18 @@
 #' CSB vacant codes
 #'
-#' @description \code{csb_vacant}returns a logical vector with TRUE for vacancy related problem codes.
+#' @description \code{csb_vacant}appends a logical vector with TRUE for vacancy related problem codes.
 #'
 #' @usage csb_vacant(.data, var, newVar, filter = FALSE)
 #'
 #' @param .data A tibble with raw CSB data
 #' @param var name of column containing problem codes
-#' @param newVar
-#' @param filter If true, returns a filtered version of the input with only vacancy related problemcodes
+#' @param newVar name of output variable to be created with vacant logical
+#' @param filter If true, returns a filtered version of the input with only vacancy related problem codes
 #'
 #' @return \code{csb_vacant}returns a logical vector with TRUE for vacancy related problem codes, or a filtered version of the input data
 #'
 #' @importFrom dplyr mutate filter
-#' @importFrom rlang quo enquo sym .data
+#' @importFrom rlang quo quo_name enquo sym .data
 #' @importFrom magrittr %>%
 #'
 #' @export
@@ -28,9 +28,24 @@ csb_vacant <- function(.data, var, newVar, filter = FALSE){
     stop('Please provide an argument for newVar')
   }
   #NSE Setup
+  paramList <- as.list(match.call())
+
+  # and quote input variables
+  if (!is.character(paramList$var)) {
+    varN <- rlang::enquo(var)
+  }
+  else if (is.character(paramList$var)) {
+    varN <- rlang::quo(!! rlang::sym(var))
+  }
+  newVarN <- rlang::quo_name(rlang::enquo(newVar))
 
   #Append logical for vacant codes
-  .data %>% dplyr::mutate(!!newVar := ifelse(!!varN %in% vacant, TRUE, FALSE)) -> .data
+  .data %>% dplyr::mutate(!!newVarN := ifelse(!!varN %in% vacant, TRUE, FALSE)) -> .data
+
+  #Filter if neccessary
+  if(isTRUE(filter)){
+    .data %>% dplyr::filter(!!varN %in% vacant) -> .data
+  }
 
   #Return the data
   return(.data)
