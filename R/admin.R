27cebb708090c9d8ad0c851164f0c69a1d622b26
Branson Fox <32143148+bransonf@users.noreply.github.com>
2018-11-27 14:31:56 -0600

    data export and test description

---
+++ R/admin.R
@@ -0,0 +1,10 @@
+#' Admin lookup table
+#'
+#'
+#' @docType data
+#'
+#' @usage data(admin)
+#'
+#' @description This is a lookup table for Problemcodes pertaining to administrative requests made to the CSB.
+#'
+"admin"
