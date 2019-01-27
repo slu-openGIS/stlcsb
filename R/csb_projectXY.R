31780bc4422da901c6a35474baee9f592c5f3065
Christopher Prener <chris.prener@slu.edu>
2019-01-26 20:36:49 -0600

    refactor to address linux testing issues

--- R/csb_projectXY.R
+++ R/csb_projectXY.R
@@ -1,15 +1,17 @@
 #' Clean CSB geo to SF object
 #'
-#' @description \code{csb_projectXY} converts SRX and SRY data into a simple features object. You can write a shapefile directly from the output of this function using sf::st_write
+#' @description \code{csb_projectXY} converts SRX and SRY data into a simple features object.
+#'     You can write a shapefile directly from the output of this function using \link{sf::st_write}.
 #'
 #' @usage csb_projectXY(.data, varX, varY, crs = NULL)
 #'
 #' @param .data A tibble with raw CSB data
-#' @param varX name of column containing SRX data
-#' @param varY name of column containing SRY data
-#' @param crs coordinate reference system for the data to be projected into
+#' @param varX Name of column containing SRX data
+#' @param varY Name of column containing SRY data
+#' @param crs Optional; coordinate reference system for the data to be projected into
 #'
-#' @return \code{csb_projectXY} returns a sf object of the input data, specifying a new crs will reproject the data.
+#' @return \code{csb_projectXY} returns a sf object of the input data, specifying a new crs will
+#'     reproject the data.
 #'
 #' @importFrom sf st_as_sf st_transform
 #' @importFrom rlang quo enquo sym :=
@@ -21,9 +23,9 @@
 #' csb_projectXY(csb, SRX, SRY, crs = 4269)
 #'
 #' @export
-csb_projectXY <- function(.data, varX, varY, crs = NULL){
-### Check input and Non-Standard evaluation
-  ## check for missing parameters of required arguments
+csb_projectXY <- function(.data, varX, varY, crs){
+
+  # check for missing parameters
   if (missing(.data)) {
     stop('Please provide an argument for .data')
   }
@@ -33,23 +35,30 @@ csb_projectXY <- function(.data, varX, varY, crs = NULL){
   if (missing(varY)) {
     stop('Please provide an argument for varY')
   }
-  ## NSE setup
+
+  # NSE setup
   varXN <- rlang::quo_name(rlang::enquo(varX))
   varYN <- rlang::quo_name(rlang::enquo(varY))
 
-  ## Check for invalid spatial data, which will error in the sf function
-  if(anyNA(.data[,varXN])){stop('Please use the csb_missing with filter = TRUE to remove invalid spatial data before using this function.')}
+  # check for invalid spatial data, which will error in the sf function
+  if(anyNA(.data[,varXN]) == TRUE){
 
-  # based on testing, I believe the CSB data to include US Survey feets coordinates by default
-  # Which is EPSG = 102696
+    stop('Please use the csb_missing with filter = TRUE to remove invalid spatial data before using this function.')
 
-  sf::st_as_sf(.data, coords = c(x = varXN, y = varYN), crs = 102696) -> sfobj
+  }
 
- # Reproject
+  # project
+  out <- sf::st_as_sf(.data, coords = c(x = varXN, y = varYN), crs = 102696)
 
- if(!is.null(crs)){sf::st_transform(sfobj, crs = crs) -> sfobj}
+  # optionally reproject
+  if(!is.null(crs)){
 
- # return the sf object
+    out <- sf::st_transform(out, crs = crs)
 
-  return(sfobj)
+  }
+
+  # return the sf object
+
+  return(out)
+
 }
