494774378637319b98d8beb8721f3704497bf675
Christopher Prener <chris.prener@slu.edu>
2019-01-31 14:10:11 -0600

    refactor csb_filter

--- R/csb_filter.R
+++ R/csb_filter.R
@@ -2,12 +2,14 @@
 #'
 #' @description \code{csb_filter} retuns observations that match any combination of 17 predefined categories.
 #'
-#' @usage csb_filter(.data, var, newVar, category)
+#' @usage csb_filter(.data, var, category)
 #'
 #' @param .data A tibble with raw CSB data
 #' @param var name of the column containg original problem code data
-#' @param newVar name of column containing categories. NULL by default, but if specified will produce a new column.
-#' @param category a vector with the unquoted name(s) of the category(s) for the function to return. You can also explicitly state quoted PROBLEMCODE(s). Valid categories are: (admin, animal, construction, debris, degrade, disturbance, event, health, landscape, law, maintenance, nature, road, sewer, traffic, vacant, waste)
+#' @param category a vector with the unquoted name(s) of the category(s) for the function to return.
+#'     You can also explicitly state quoted PROBLEMCODE(s). Valid categories are: admin, animal,
+#'     construction, debris, degrade, disturbance, event, health, landscape, law, maintenance, nature,
+#'     road, sewer, traffic, vacant, and waste.
 #'
 #' @return \code{csb_filter} returns data with an additional variable for an intelligible category for CSB requests.
 #'
@@ -16,11 +18,11 @@
 #' @importFrom magrittr %>%
 #'
 #' @examples
-#' csb_filter(january_2018, PROBLEMCODE, Category, category = c(cat_waste, cat_debris))
-#' csb_filter(january_2018, PROBLEMCODE, category = cat_vacant)
+#' csb_filter(january_2018, var = PROBLEMCODE, category = c(cat_waste, cat_debris))
+#' csb_filter(january_2018, var = PROBLEMCODE, category = cat_vacant)
 #'
 #' @export
-csb_filter <- function(.data, var, newVar, category){
+csb_filter <- function(.data, var, category){
 
   # check for missing parameters
   if (missing(.data)){
@@ -29,10 +31,6 @@ csb_filter <- function(.data, var, newVar, category){
 
   if (missing(var)){
     stop('Please provide an argument for var')
-  }
-
-  if (missing(newVar)){
-    message("No argument set for `newVar` If you would like to append a category variable, please set an argument for `newVar`")
   }
 
   if (missing(category)){
@@ -50,9 +48,7 @@ csb_filter <- function(.data, var, newVar, category){
     varN <- rlang::quo(!! rlang::sym(var))
   }
 
-  newVarN <- rlang::quo_name(rlang::enquo(newVar))
-
-  # initialize a list of valid category arguments
+  # create a list of valid category arguments
   validCategory = c(
     stlcsb::cat_admin,
     stlcsb::cat_animal,
@@ -73,26 +69,14 @@ csb_filter <- function(.data, var, newVar, category){
     stlcsb::cat_waste
   )
 
-  # to check if category input is valid
+  # check if category input is valid
   if(!all(category %in% validCategory)){
     stop("Category contains an invalid argument, please see `?csb_filter` for help")
   }
 
-  # Filter function
-  .data <- filter(.data, !!varN %in% category)
-
-  # categorize data
-  if(!missing(newVar)){
-
-    .data <- csb_categorize(.data, !!varN, !!newVarN)
-
-  }
-
-  if(all(stlcsb::cat_vacant %in% category)){
-    message("specifying vacant will supersede the original category")
-
-    .data <- dplyr::mutate(.data, !!newVarN := ifelse(!!varN %in% stlcsb::cat_vacant,"Vacant", .data[[newVarN]]))
-  }
+  # filter function
+  .data %>%
+    filter(!!varN %in% category) -> .data
 
   # return the final data
   return(.data)
--- tests/testthat/test_filter.R
+++ tests/testthat/test_filter.R
@@ -12,13 +12,6 @@ test_that("Missing input errors triggered", {
   expect_error(csb_filter(test_data, PROBLEMCODE, category = "non-real category"), "Category contains an invalid argument, please see `?csb_filter` for help", fixed = TRUE)
 })
 
-# test messaging ---------------------------------
-
-test_that("Messaging works properly", {
-  expect_message(csb_filter(test_data, PROBLEMCODE, category = cat_waste), "No argument set for `newVar` If you would like to append a category variable, please set an argument for `newVar`")
-  expect_message(csb_filter(test_data, PROBLEMCODE, newVar = vacant, category = cat_vacant), "specifying vacant will supersede the original category")
-})
-
 # test filtering ----------------------------------
 output <- unlist(csb_filter(test_data, "PROBLEMCODE", category = cat_waste)[1])
 
