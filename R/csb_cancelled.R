c9cd3240919739a4d2873de8f7bec58b2992cc2e
Christopher Prener <chris.prener@slu.edu>
2019-01-31 15:45:43 -0600

    create csb_cancelled

---
+++ R/csb_cancelled.R
@@ -0,0 +1,59 @@
+#' Remove Cancelled Observations
+#'
+#' @description Subsets data to remove any call with a date and time cancelled.
+#'
+#' @usage csb_cancelled(.data, var, drop = TRUE)
+#'
+#' @param .data A tbl
+#' @param var Name of the column containing cancellation timestamps; by default
+#'     this should be \code{DATECANCELLED}
+#' @param drop Optionally remove the now empty column that had contained cancellation
+#'     date and time if \code{TRUE}
+#'
+#' @return Returns a tibble with the rows containing dates and times for the
+#'     given variable removed.
+#'
+#' @importFrom dplyr filter
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#'
+#' @examples
+#' csb_cancelled(january_2018, var = "DATECANCELLED")
+#' csb_cancelled(january_2018, var = "DATECANCELLED", drop = FALSE)
+#'
+#' @export
+csb_cancelled <- function(.data, var, drop = TRUE){
+
+  # global variables
+  ...cancelled = NULL
+
+  # check for missing parameters
+  if (missing(.data)) {
+    stop('Please provide an argument for .data')
+  }
+
+  if (missing(var)) {
+    stop('Please provide the name of the variable containing the cancellation timestamps.')
+  }
+
+  # save parameters to list for quoting
+  paramList <- as.list(match.call())
+
+  # quote input variables
+  varNQ <- rlang::quo_name(rlang::enquo(var))
+
+  # filter
+  .data %>%
+    dplyr::rename(...cancelled = !!varNQ) %>%
+    dplyr::filter(is.na(...cancelled) == TRUE) %>%
+    dplyr::rename(!!varNQ := ...cancelled) -> out
+
+  # optionally remove the now empty column
+  if (drop == TRUE){
+    out <- dplyr::select(out, -!!varNQ)
+  }
+
+  # return output
+  return(out)
+
+}
---
+++ tests/testthat/test_cancelled.R
@@ -0,0 +1,29 @@
+context("test csb_cancelled")
+
+# load test data --------------------------------
+
+data(january_2018, package = "stlcsb")
+
+# test errors -----------------------------------
+
+test_that("Missing input errors triggered", {
+  expect_error(csb_cancelled(), "Please provide an argument for .data")
+  expect_error(csb_cancelled(var = "DATECANCELLED"), "Please provide an argument for .data")
+  expect_error(csb_cancelled(test), "Please provide the name of the variable containing the cancellation timestamps.")
+})
+
+# test results ------------------------------------------------
+
+test1 <- csb_cancelled(january_2018, var = DATECANCELLED)
+
+test_that("data are dropped appropriately", {
+  expect_equal(nrow(test1), 1647)
+  expect_equal(ncol(test1), 18)
+})
+
+test2 <- csb_cancelled(january_2018, var = "DATECANCELLED", drop = FALSE)
+
+test_that("data are dropped appropriately", {
+  expect_equal(nrow(test2), 1647)
+  expect_equal(ncol(test2), 19)
+})
