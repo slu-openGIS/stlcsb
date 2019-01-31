4dde595294390ec09877d79dd094fceff81fcc39
Christopher Prener <chris.prener@slu.edu>
2019-01-31 13:21:04 -0600

    clea up csb_filter

--- R/csb_filter.R
+++ R/csb_filter.R
@@ -21,22 +21,28 @@
 #'
 #' @export
 csb_filter <- function(.data, var, newVar, category){
-#MISSING AND NSE SETUP
+
+  # check for missing parameters
   if (missing(.data)){
     stop('Please provide an argument for .data')
   }
+
   if (missing(var)){
     stop('Please provide an argument for var')
   }
+
   if (missing(newVar)){
     message("No argument set for `newVar` If you would like to append a category variable, please set an argument for `newVar`")
   }
+
   if (missing(category)){
     stop('Please provide an argument for category')
   }
 
+  # save parameters to list
   paramList <- as.list(match.call())
 
+  # quote input variables
   if (!is.character(paramList$var)) {
     varN <- rlang::enquo(var)
   }
@@ -46,30 +52,49 @@ csb_filter <- function(.data, var, newVar, category){
 
   newVarN <- rlang::quo_name(rlang::enquo(newVar))
 
-  #-------------------------------------------------------------------------------------------------------------
-
   # initialize a list of valid category arguments
-  validCategory = c(stlcsb::cat_admin,stlcsb::cat_animal,stlcsb::cat_construction,stlcsb::cat_debris,stlcsb::cat_degrade,stlcsb::cat_disturbance,stlcsb::cat_event,stlcsb::cat_health,stlcsb::cat_landscape,stlcsb::cat_law,stlcsb::cat_maintenance,stlcsb::cat_nature,stlcsb::cat_road,stlcsb::cat_sewer,stlcsb::cat_traffic,stlcsb::cat_vacant,stlcsb::cat_waste)
+  validCategory = c(
+    stlcsb::cat_admin,
+    stlcsb::cat_animal,
+    stlcsb::cat_construction,
+    stlcsb::cat_debris,
+    stlcsb::cat_degrade,
+    stlcsb::cat_disturbance,
+    stlcsb::cat_event,
+    stlcsb::cat_health,
+    stlcsb::cat_landscape,
+    stlcsb::cat_law,
+    stlcsb::cat_maintenance,
+    stlcsb::cat_nature,
+    stlcsb::cat_road,
+    stlcsb::cat_sewer,
+    stlcsb::cat_traffic,
+    stlcsb::cat_vacant,
+    stlcsb::cat_waste
+  )
+
   # to check if category input is valid
-  if(!all(category %in% validCategory)){stop("Category contains an invalid argument, please see `?csb_filter` for help")}
-
-  #-------------------------------------------------------------------------------------------------------------
-  # Filter function
-.data %>%
-  filter(!!varN %in% category) -> .data
-
-  #-------------------------------------------------------------------------------------------------------------
-  # categorizing
-
-## categorize data
-if(!missing(newVar)){
-  csb_categorize(.data, !!varN, !!newVarN) -> .data
-
-  if(all(stlcsb::cat_vacant %in% category)){message("specifying vacant will supersede the original category")
-    .data %>% dplyr::mutate(!!newVarN := ifelse(!!varN %in% stlcsb::cat_vacant,"Vacant", .data[[newVarN]])) -> .data
+  if(!all(category %in% validCategory)){
+    stop("Category contains an invalid argument, please see `?csb_filter` for help")
   }
-}
 
-# return the final data
-return(.data)
+  # Filter function
+  .data <- filter(.data, !!varN %in% category)
+
+  # categorize data
+  if(!missing(newVar)){
+
+    .data <- csb_categorize(.data, !!varN, !!newVarN)
+
+  }
+
+  if(all(stlcsb::cat_vacant %in% category)){
+    message("specifying vacant will supersede the original category")
+
+    .data <- dplyr::mutate(.data, !!newVarN := ifelse(!!varN %in% stlcsb::cat_vacant,"Vacant", .data[[newVarN]]))
+  }
+
+  # return the final data
+  return(.data)
+
 }
